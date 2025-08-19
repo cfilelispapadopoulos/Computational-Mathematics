abd5408475a7e800e1e2a74d117e8fd87eb38f0b
Christos Papadopoulos Filelis <137081674+cfilelispapadopoulos@users.noreply.github.com>
2025-08-19 15:15:43 +0100

    Update prec.cpp

--- AFIIM/src/prec.cpp
+++ AFIIM/src/prec.cpp
@@ -18,7 +18,8 @@
 //
 //
 // References
-// [1] C. K. Filelis - Papadopoulos (2024). Adaptive Factored Incomplete Inverse Matrices. In Review.
+// [1] C. K. Filelis - Papadopoulos (2024). Adaptive Factored Incomplete Inverse Matrices. 
+//     Numer Linear Algebra Appl, 32: e2603. https://doi.org/10.1002/nla.2603
 
 
 
